ba9ed4c83e8c34323d0d1c0fe4c6adc7e0e2d5b5
Volodymyr Ustilkin <volodymyr.ustilkin@gmail.com>
2021-04-12 00:38:34 +0300

    code cleanup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,25 +2,15 @@ import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {AppRouting} from './app-routing';
 import {AppComponent} from './app.component';
-import {NavMenuComponent} from './core/components/nav-menu/nav-menu.component';
-import {HeaderComponent} from './core/components/header/header.component';
-import {HttpClientModule, HTTP_INTERCEPTORS} from '@angular/common/http';
-import {NavMenuNewComponent} from './core/components/nav-menu-new/nav-menu-new.component';
-import {FullSizeHeaderComponent} from './core/components/full-size-header/full-size-header.component';
-import {NavMenuMobileComponent} from './core/components/nav-menu-mobile/nav-menu-mobile.component';
+import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
 import {PageNotFoundComponent} from './core/components/page-not-found/page-not-found.component';
-import { AuthInterceptor } from './core/interceptors/auth.interceptor';
-import { NavigationUiComponent } from './core/components/navigation-new-design/navigation-ui.component';
-import { HeaderUiComponent } from './core/components/header-new-design/header-ui.component';
+import {AuthInterceptor} from './core/interceptors/auth.interceptor';
+import {NavigationUiComponent} from './core/components/navigation-new-design/navigation-ui.component';
+import {HeaderUiComponent} from './core/components/header-new-design/header-ui.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavMenuComponent,
-    HeaderComponent,
-    NavMenuNewComponent,
-    FullSizeHeaderComponent,
-    NavMenuMobileComponent,
     PageNotFoundComponent,
     NavigationUiComponent,
     HeaderUiComponent,
@@ -30,7 +20,7 @@ import { HeaderUiComponent } from './core/components/header-new-design/header-ui
     HttpClientModule,
     AppRouting,
   ],
-  providers : [{
+  providers: [{
     provide: HTTP_INTERCEPTORS, // Injection Token
     useClass: AuthInterceptor, // Interceptor class
     multi: true, // we added array
