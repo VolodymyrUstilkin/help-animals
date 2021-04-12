d496cc59f97ed34b6acc1957a25f41557905b7bd
Volodymyr Ustilkin <volodymyr.ustilkin@gmail.com>
2021-04-12 15:34:17 +0300

    fix environment bug

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 // const apiServerHost = 'http://ec2-13-49-41-155.eu-north-1.compute.amazonaws.com:3000';
-const serverHost = 'http://help-animals.hopto.org:3000';
+const apiServerHost = 'http://help-animals.hopto.org:3000';
 
-const apiUrl = serverHost + '/api/v1';
-const activeAdminApiUrl = serverHost + '/admin';
+const apiUrl = apiServerHost + '/api/v1';
+const activeAdminApiUrl = apiServerHost + '/admin';
 
 export const environment = {
   production: true,
-  serverHost,
+  apiServerHost,
   apiUrl,
   activeAdminApiUrl
 };
