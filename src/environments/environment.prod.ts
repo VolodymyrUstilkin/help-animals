4c7ca1fcbf4277f5f98a3a05287859ac62d3448e
Volodymyr Ustilkin <volodymyr.ustilkin@gmail.com>
2021-04-14 10:10:34 +0300

    update apiServerHost to https

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 // const apiServerHost = 'http://ec2-13-49-41-155.eu-north-1.compute.amazonaws.com:3000';
-const apiServerHost = 'http://help-animals.pp.ua:3000';
+const apiServerHost = 'https://help-animals.pp.ua:3000';
 
 const apiUrl = apiServerHost + '/api/v1';
 const activeAdminApiUrl = apiServerHost + '/admin';
