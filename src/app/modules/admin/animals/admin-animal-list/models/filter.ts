feddbb6bfbfaafad18b0f9e8c656df77d5b640a4
Volodymyr Ustilkin <volodymyr.ustilkin@gmail.com>
2021-04-19 19:36:59 +0300

    add filter params to admin-animal-list.component

--- src/app/modules/admin/animals/admin-animal-list/models/filter.ts
+++ src/app/modules/admin/animals/admin-animal-list/models/filter.ts
@@ -1,32 +1,58 @@
-export interface IFilter {
-  ageMin: string;
-  ageMax: string;
-  responsiblePerson: string;
-  sterilization: boolean;
-  rabiesVaccination: boolean;
-  complexVaccination: boolean;
-  animalHasFamily: boolean;
+enum EBooleanTypes {
+  notUse = '',
+  true = 'true',
+  false = 'false'
 }
 
-export class Filter implements IFilter {
+interface IFilterRequest {
+  filter_age_min: string;
+  filter_age_max: string;
+  filter_responsible_person: string;
+  filter_features: string;
+  filter_sterilization: EBooleanTypes;
+  filter_rabies_vaccination: EBooleanTypes;
+  filter_complex_vaccination: EBooleanTypes;
+  filter_animal_has_family: EBooleanTypes;
+}
+
+interface IGetQueryParams {
+  getQueryParams(): { [param: string]: string };
+}
+
+export class Filter implements IGetQueryParams {
   showFilter = false;
+
   ageMin = '';
   ageMax = '';
   responsiblePerson = '';
-  sterilization = false;
-  rabiesVaccination = false;
-  complexVaccination = false;
-  animalHasFamily = false;
+  features = '';
+  sterilization: EBooleanTypes = EBooleanTypes.notUse;
+  rabiesVaccination = EBooleanTypes.notUse;
+  complexVaccination = EBooleanTypes.notUse;
+  animalHasFamily = EBooleanTypes.notUse;
+
+
 
   getQueryParams(): { [param: string]: string } {
-    return {
+    const ret: IFilterRequest = {
       filter_age_min: this.ageMin,
       filter_age_max: this.ageMax,
       filter_responsible_person: this.responsiblePerson,
-      filter_sterilization: this.sterilization.toString(),
-      filter_rabies_vaccination: this.rabiesVaccination.toString(),
-      filter_complex_vaccination: this.complexVaccination.toString(),
-      filter_animal_has_family: this.animalHasFamily.toString(),
+      filter_features: this.features,
+      filter_sterilization: this.sterilization,
+      filter_rabies_vaccination: this.rabiesVaccination,
+      filter_complex_vaccination: this.complexVaccination,
+      filter_animal_has_family: this.animalHasFamily,
     };
+
+    return ret as unknown as { [param: string]: string };
+  }
+
+  changeShowFilter(): void {
+    this.showFilter = !this.showFilter;
+  }
+
+  getBooleanTypes(): typeof EBooleanTypes {
+    return EBooleanTypes;
   }
 }
